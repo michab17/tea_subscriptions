8f6e651da19beb93cde90526563998bdb6d0ba68
Micha Bernhard <mib70@pitt.edu>
2022-02-23 10:46:54 -0700

    Create all required endpoints

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,13 +1,21 @@
 class SubscriptionsController < ApplicationController
   def index
-
+    customer = Customer.find(params[:customer_id])
+    render json: SubscriptionSerializer.new(customer.subscriptions)
   end
 
   def create
-    
+    render json: SubscriptionSerializer.new(Subscription.create(subscription_params))
   end
 
   def update
+    subscription = Subscription.update(params[:id], status: 'Canceled')
+    render json: SubscriptionSerializer.new(subscription)
+  end
 
+  private
+
+  def subscription_params
+    params.permit(:customer_id, :tea_id, :price, :status, :frequency)
   end
 end
